3776682de4d6637d2fd2e06e7dc42e05331bb6ec
georgejeffery <georgejeffery@gmail.com>
2019-02-20 11:06:36 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,39 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (type) => {
+    this.setState({
+      filters: {
+        type: type
+      }
+    })
+  }
+
+  fetchPets = (type) => {
+    if (type === "all") {
+      fetch('/api/pets').then((resp) => { return resp.json() }).then((pets) => { this.savePets(pets) })
+    } else {
+      fetch(`/api/pets?type=${type}`).then((resp) => { return resp.json() }).then((pets) => { this.savePets(pets) })
+    }
+  }
+  savePets = (pets) => {
+    this.setState({
+      pets: pets
+    })
+  }
+
+  onFindPetsClick = () => {
+    this.fetchPets(this.state.filters.type)
+  }
+
+  onAdoptPet = (petID) => {
+    let selectedPet = this.state.pets.find(pet => pet.id === petID)
+    selectedPet.isAdopted = true
+    this.setState({
+      pets: [...this.state.pets]
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +57,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 import Pet from './Pet'
 
+
+
 class PetBrowser extends React.Component {
+  petMap = () => {
+    let petList = this.props.pets.map((pet) => { return <Pet pet={pet} onAdoptPet={this.props.onAdoptPet} /> })
+    return petList
+  }
+
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards">
+      {this.petMap()}
+    </div>
   }
 }
 
